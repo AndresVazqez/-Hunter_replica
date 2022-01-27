e1e7a841df60c8bdf8137b5503cd0f29b386cc29
AndresVazqez <aavsz0811@gmail.com>
2022-01-28 00:09:17 +0100

    Act

--- hunter.js
+++ hunter.js
@@ -1,2 +1,21 @@
-const header$$ = document.querySelector(".header_info");
+
+const menu$$ = document.querySelector(".header_menu-icon");
+const drop$$ = document.querySelector(".header_nav-section")
+
+const dropNav = () => {  
+    
+    drop$$.classList.toggle("dropMenu");
+}
+
+menu$$.addEventListener("click", dropNav);
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth >= 992 ){
+        drop$$.className= "header_nav-section";     
+
+    } else {
+
+    }                  
+    
+});
 
